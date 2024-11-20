8809c7096ceafac313043baad69c348e42d24081
justachillguy69 <macdonaldetienne@gmail.com>
2024-11-20 09:55:38 -0500

    tst

---
+++ prog_final/prog_final/Adherent.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_final
+{
+    internal class Adherent : INotifyPropertyChanged
+    {
+
+        int idAdherent;
+        string nom;
+        string prenom;
+        string adresse;
+        string dateNaissance;
+
+        public Adherent()
+        {
+            this.idAdherent = -1;
+            this.nom = "nom";
+            this.prenom = "prenom";
+            this.adresse = "adresse";
+            this.dateNaissance = "dateNaissance";
+        }
+
+        public Adherent(int idAdherent, string nom, string prenom, string adresse, string dateNaissance)
+        {
+            this.idAdherent = idAdherent;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.adresse = adresse;
+            this.dateNaissance = dateNaissance;
+        }
+        public int IdAdherent
+        {
+            get => idAdherent;
+            set
+            {
+                idAdherent = value;
+                this.OnPropertyChanged(nameof(IdAdherent));
+            }
+
+        }
+
+        public string Nom
+        {
+            get => nom;
+            set
+            {
+                nom = value;
+                this.OnPropertyChanged(nameof(Nom));
+            }
+
+        }
+
+        public string Prenom
+        {
+            get => prenom;
+            set
+            {
+                prenom = value;
+                this.OnPropertyChanged(nameof(Prenom));
+            }
+
+        }
+
+        public string Adresse
+        {
+            get => adresse;
+            set
+            {
+                adresse = value;
+                this.OnPropertyChanged(nameof(Adresse));
+            }
+
+        }
+
+        public string DateNaissance
+        {
+            get => dateNaissance;
+            set
+            {
+                dateNaissance = value;
+                this.OnPropertyChanged(nameof(DateNaissance));
+            }
+
+        }
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ prog_final/prog_final/SingletonAdherent.cs
@@ -0,0 +1,98 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_final
+{
+    internal class SingletonAdherent
+    {
+        MySqlConnection con; //test
+        ObservableCollection<Adherent> liste_des_adherent;
+        static SingletonAdherent instance = null;
+
+        public SingletonAdherent()
+        {
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=420345ri_gr00001_2366599-etienne-mac-donald;Uid=2366599;Pwd=2366599;");
+            liste_des_adherent = new ObservableCollection<Adherent>();
+        }
+
+        // SINGLETON
+        public static SingletonAdherent getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SingletonAdherent();
+            }
+            return instance;
+        }
+
+        public ObservableCollection<Adherent> getListe_des_adherent()
+        {
+            return liste_des_adherent;
+        }
+
+        public async void ajoutCSV()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.FileTypeFilter.Add(".csv");
+
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(GestionWindow.mainWindow);
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
+
+            Windows.Storage.StorageFile monFichier = await picker.PickSingleFileAsync();
+            if (monFichier == null)
+            {
+                return;
+            }
+
+            var lignes = await Windows.Storage.FileIO.ReadLinesAsync(monFichier);
+
+            foreach (var ligne in lignes)
+            {
+                var v = ligne.Split(";");
+
+                addAdherent(v[0], v[1], v[2], v[3]);
+            }
+        }
+
+        public void addAdherent(string _nom, string _prenom, string _adresse, string _dateNaissance)
+        {
+            try
+            {
+                MySqlCommand commande = new MySqlCommand();
+                commande.Connection = con;
+                commande.CommandText = "INSERT INTO adherent (nom, prenom, adresse, dateNaissance) VALUES (@nom, @prenom, @adresse, @dateNaissance);";
+
+                commande.Parameters.AddWithValue("@nom", _nom);
+                commande.Parameters.AddWithValue("@prenom", _prenom);
+                commande.Parameters.AddWithValue("@adresse", _adresse);
+                commande.Parameters.AddWithValue("@dateNaissance", _dateNaissance);
+
+                con.Open();
+                commande.ExecuteNonQuery();
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    con.Close();
+                }
+
+                Console.WriteLine(ex.Message);
+            }
+            //getProduit();
+        }
+
+
+
+    }
+}
