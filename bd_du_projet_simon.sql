5c01518f828028c7b35944f448dba057c1f3afb0
Y0ungM3tr0 <simcartier1@outlook.com>
2024-11-22 14:46:08 -0500

    Modifications BD
    Ajout de triggers

--- bd_du_projet_simon.sql
+++ bd_du_projet_simon.sql
@@ -14,4 +14,61 @@ DELIMITER ;
 
 
 -- 3.2
--- 
+-- Gérer le nombre de places disponibles dans chaque séances. Ajouter un participant (adhérent)
+-- augmente le nombre de places (incrémentation)
+DELIMITER //
+CREATE TRIGGER check_nbr_places_disponibles
+BEFORE INSERT
+ON reservation
+FOR EACH ROW
+BEGIN
+    DECLARE places_disponibles INT;
+
+    SELECT nbr_place_disponible - nbr_inscription
+    INTO places_disponibles
+    FROM seance
+    WHERE idSeance = NEW.idSeance;
+
+    IF places_disponibles <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT="Impossible d'ajouter la réservation: la séance est complète.";
+    END IF ;
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER add_place_chq_adherent
+AFTER INSERT
+ON reservation
+FOR EACH ROW
+BEGIN
+    UPDATE seance
+    SET nbr_inscription = nbr_inscription + 1
+    WHERE idSeance = NEW.idSeance;
+END//
+DELIMITER ;
+
+
+-- 3.3
+-- Insérer les participants (adhérents) dans une séance (réservation)
+DELIMITER //
+CREATE TRIGGER check_nbr_places_disponibles
+BEFORE INSERT
+ON reservation
+FOR EACH ROW
+BEGIN
+    DECLARE places_disponibles INT;
+
+    SELECT nbr_place_disponible - nbr_inscription
+    INTO places_disponibles
+    FROM seance
+    WHERE idSeance = NEW.idSeance;
+
+    IF places_disponibles <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT="Impossible d'ajouter la réservation: la séance est complète.";
+    ELSE
+        INSERT INTO reservation VALUES (NULL, idSeance, matricule);
+    END IF ;
+END//
+DELIMITER ;
